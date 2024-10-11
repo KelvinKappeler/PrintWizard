f8bbe6eb7be98718cbebcdc0187e0dfb145a39fc
KelvinKappeler <kelvin.kappeler@epfl.ch>
2024-10-11 16:34:12 +0200

    Avancements

--- Frontend/src/js/def.js
+++ Frontend/src/js/def.js
@@ -1,13 +1,47 @@
 class TraceElement {
+    constructor(lineNumber, content, depth) {
+        this.lineNumber = lineNumber;
+        this.content = content;
+        this.depth = depth;
+    }
+
+    show(traceArea) {}
+}
+
+export class Statement extends TraceElement {
+    constructor(lineNumber, content, depth, objects) {
+        super(lineNumber, content, depth);
+        this.objects = objects;
+    }
+
+    show(traceArea) {
+        traceArea.addLine(this.lineNumber, "  ".repeat(this.depth) + this.content, false);
+    }
 }
 
 export class FunctionTrace extends TraceElement {
-    constructor(name, args, returnVal, line, content) {
-        super();
+    constructor(lineNumber, content, depth, name, args, returnVal) {
+        super(lineNumber, content, depth);
         this.name = name;
         this.args = args;
         this.returnVal = returnVal;
-        this.line = line;
-        this.content = content;
     }
+
+    show(traceArea) {
+        traceArea.addLine(this.lineNumber, "  ".repeat(this.depth) + this.name + " {", true);
+
+        for (let statement of this.content) {
+            statement.show(traceArea);
+        }
+
+        traceArea.addLine("-", "  ".repeat(this.depth) + "}", false);
+    }
+}
+
+export class Loop extends TraceElement {
+
+}
+
+export class Conditional extends TraceElement {
+
 }
--- Frontend/src/js/trace.js
+++ Frontend/src/js/trace.js
@@ -1,22 +1,72 @@
-import { FunctionTrace } from './def.js';
+import {FunctionTrace, Statement} from './def.js';
 
-const lineNumbers = document.querySelector('.lineNumbers');
-const triangles = document.querySelector('.traceTriangles');
-const traceContent = document.querySelector('.traceContent');
+const lineNumbersAreaSelector = '.lineNumbers';
+const trianglesAreaSelector = '.traceTriangles';
+const traceContentAreaSelector = '.traceContent';
 
-function addTraceLine(ft) {
-    lineNumbers.appendChild(document.createTextNode(ft.line));
-    lineNumbers.appendChild(document.createElement('br'));
+export class TraceArea {
+    constructor(lineNumbersArea, trianglesArea, traceContentArea) {
+        this.lineNumbersArea = new LineNumbersArea(lineNumbersArea);
+        this.trianglesArea = new TrianglesArea(trianglesArea);
+        this.traceContentArea = new TraceContentArea(traceContentArea);
+    }
 
-    /*let triangle = document.createElement('li');
-    triangle.innerHTML = ft.triangle;
-    triangles.appendChild(triangle);*/
+    addLine(lineNumber, content, canFold) {
+        this.lineNumbersArea.addLine(lineNumber);
+        this.traceContentArea.addContent(content);
+        this.trianglesArea.addTriangle(canFold);
+    }
+}
 
-    traceContent.appendChild(document.createTextNode(ft.content));
-    //traceContent.insertAdjacentText('afterbegin', '\n');
+export class LineNumbersArea {
+    constructor(selector) {
+        this.lineNumbers = document.querySelector(selector);
+    }
+
+    addLine(lineNumber) {
+        this.lineNumbers.appendChild(document.createTextNode(lineNumber));
+        this.lineNumbers.appendChild(document.createElement('br'));
+    }
+}
+
+export class TrianglesArea {
+    constructor(selector) {
+        this.triangles = document.querySelector(selector);
+    }
+
+    addTriangle(canFold) {
+        if (canFold) {
+            let triangle = document.createElement('i');
+            triangle.classList.add('bi');
+            triangle.classList.add('bi-chevron-down');
+            this.triangles.appendChild(triangle);
+            this.triangles.appendChild(document.createElement('br'));
+        } else {
+            this.triangles.appendChild(document.createElement('br'));
+        }
+    }
+
+}
+
+export class TraceContentArea {
+    constructor(selector) {
+        this.lineContent = document.querySelector(selector)
+    }
+
+    addContent(lineContent) {
+        this.lineContent.appendChild(document.createTextNode(lineContent))
+        this.lineContent.appendChild(document.createElement('br'))
+    }
 }
 
 window.addEventListener('load', function() {
-    let ft = new FunctionTrace("blabla", null, 1, 10, "content");
-    addTraceLine(ft);
+    let statements = [
+        new Statement(2, "System.out.println(\"test\")", 1, []),
+        new FunctionTrace(9, [
+            new Statement(10, "int i = 0;", 3, []),
+            new Statement(11, "i++;", 3, []),
+        ], 2, "testFunction", null, null),
+    ];
+    let ft2 = new FunctionTrace(1, statements, 0,  "main", null, null);
+    ft2.show(new TraceArea(lineNumbersAreaSelector, trianglesAreaSelector, traceContentAreaSelector));
 });
