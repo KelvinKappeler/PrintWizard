426d3b6109f1f1d0e62fdc50b1b553917f3ed7b7
bjolidon <bastien.jolidon@epfl.ch>
2024-10-11 14:06:20 +0200

    fix trace

--- Frontend/src/js/trace.js
+++ Frontend/src/js/trace.js
@@ -5,7 +5,7 @@ const triangles = document.querySelector('.traceTriangles');
 const traceContent = document.querySelector('.traceContent');
 
 function addTraceLine(ft) {
-    lineNumbers.appendChild(document.createTextNode(ft.lineNumber));
+    lineNumbers.appendChild(document.createTextNode(ft.line));
     lineNumbers.appendChild(document.createElement('br'));
 
     /*let triangle = document.createElement('li');
@@ -13,8 +13,10 @@ function addTraceLine(ft) {
     triangles.appendChild(triangle);*/
 
     traceContent.appendChild(document.createTextNode(ft.content));
-    traceContent.insertAdjacentText('afterbegin', '\n');
+    //traceContent.insertAdjacentText('afterbegin', '\n');
 }
 
-let ft = new FunctionTrace("blabla", null, 1, 10, "content");
-addTraceLine(ft);
+window.addEventListener('load', function() {
+    let ft = new FunctionTrace("blabla", null, 1, 10, "content");
+    addTraceLine(ft);
+});
