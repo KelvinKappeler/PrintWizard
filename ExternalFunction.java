fe1101bbbdea8007b4210f0010486ecaa24a0581
KelvinKappeler <kelvin.kappeler@epfl.ch>
2024-11-09 15:18:09 +0100

    Update ExternalFunction.java

--- ExternalFunction.java
+++ ExternalFunction.java
@@ -4,6 +4,6 @@ public class ExternalFunction {
 
         long currentTime = System.currentTimeMillis();
 
-        long mathTest = System.out.println(Math.sqrt(Math.random()).toString());
+        System.out.println(Math.sqrt(Math.random()).toString());
     }
 }
