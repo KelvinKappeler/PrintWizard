d1495e1c2f296867ea7f823af552fcce11fb56d1
KelvinKappeler <kelvin.kappeler@gmail.com>
2024-11-09 14:39:20 +0100

    Add examples

---
+++ Loops.java
@@ -0,0 +1,16 @@
+public class Loops {
+    public static void main(String[] args) {
+        int compteur = 0;
+        for (int i = 0; i < 5; i++) {
+            compteur++;
+        }
+        
+        while (compteur != 0) {
+            for (int j = 0; j < compteur; j++) {
+                System.out.println("Bonjour");
+            }
+            
+            compteur--;
+        }
+    }
+}
